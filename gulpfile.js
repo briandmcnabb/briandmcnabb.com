6e488c9610f63ffbaf38de5033823a5165ce0ff8
Brian McNabb <brian.d.mcnabb@gmail.com>
2015-06-14 20:01:40 -0400

    Integrate harp and browserSync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,67 @@
 var gulp        = require('gulp');
+var harp        = require('harp')
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 var deploy      = require('gulp-gh-pages');
+var cp          = require('child_process');
 
 /**
- * Default Task
+ * Serve the Harp Site
+ */
+gulp.task('serve', function () {
+  harp.server(__dirname, {
+    port: 9000
+  }, function () {
+    browserSync({
+      proxy: "localhost:9000",
+      open: false,
+      /* Hide the notification. It gets annoying */
+      notify: {
+        styles: ['opacity: 0', 'position: absolute']
+      }
+    });
+    /**
+     * Watch for sass changes, tell BrowserSync to refresh main.css
+     */
+    gulp.watch("public/**/*.scss", function () {
+      reload("main.css", {stream: true});
+    });
+    /**
+     * Watch for all other changes, reload the whole page
+     */
+    gulp.watch(["public/**/*.jade", "public/**/*.json", "public/**/*.md"], function () {
+      reload();
+    });
+  })
+});
+
+/**
+ * Serve the site in production
  */
 
-gulp.task('default', function() {
-  console.log("Gulp lol look at me being rad.");
+gulp.task('production', function (done) {
+  cp.exec('NODE_ENV=production sudo harp server --port 80', {stdio: 'inherit'})
+    .on('close', done)
+});
+
+/**
+ * Build the Harp Site
+ */
+gulp.task('build', function (done) {
+  cp.exec('harp compile . dist', {stdio: 'inherit'})
+    .on('close', done)
 });
 
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
   return gulp.src("./dist/**/*")
     .pipe(deploy())
 });
+
+/**
+ * Default task, running `gulp` will fire up the Harp site,
+ * launch BrowserSync & watch files.
+ */
+gulp.task('default', ['serve']);
